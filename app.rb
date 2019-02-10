d5ecdfddaefbeb701558472f332026a654d68697
John Mannion <g00340314@gmit.ie>
2019-02-10 19:41:26 +0000

    fixing empty space and number input

--- app.rb
+++ app.rb
@@ -45,10 +45,15 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-  if(!@game.guess(letter))
-		flash[:message]="You have already used that letter"
-  end
-	redirect '/show'
+    begin
+      if(!@game.guess(letter))
+    		flash[:message]="You have already used that letter"
+      end
+    rescue ArgumentError => _
+    
+    flash[:message] = "Invalid Guess !!"
+    end
+  	redirect '/show'
 
   end
   
